16784b18228c353b8e92ea6b8eb72e2310f3e493
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2014-08-24 08:52:27 +0200

    Initial commit for grunt-scrape task.

---
+++ tasks/scrape.js
@@ -0,0 +1,15 @@
+var grunt = require('grunt');
+var Scraper = require('scrape');
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('scrape', 'Extract data from web pages.', function() {
+    var config = this.data;
+    var done = this.async();
+
+    Scraper.scrape(config)
+      .then(function() {
+        done();
+      });
+  });
+};
